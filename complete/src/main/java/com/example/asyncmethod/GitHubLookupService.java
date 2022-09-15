e3039a980a9d69dd3a651cb10d34058f6945794d
Omollo <KEN20969@kcbgroup.com>
2022-09-15 08:41:17 +0300

    async service

--- complete/src/main/java/com/example/asyncmethod/GitHubLookupService.java
+++ complete/src/main/java/com/example/asyncmethod/GitHubLookupService.java
@@ -3,6 +3,7 @@ package com.example.asyncmethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -27,7 +28,15 @@ public class GitHubLookupService {
 		User results = restTemplate.getForObject(url, User.class);
 		// Artificial delay of 1s for demonstration purposes
 		Thread.sleep(1000L);
+		logger.info("*****************Calling client with response ***************{}",results);
+		invokeCallBack(results);
 		return CompletableFuture.completedFuture(results);
+	}
+
+	public ResponseEntity<?> invokeCallBack(User user){
+		String url=String.format("http://localhost:8089/api/users");
+		String userResult= restTemplate.postForObject(url,user,String.class);
+		return ResponseEntity.ok(userResult);
 	}
 
 }
